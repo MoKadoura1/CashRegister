06c27dc42452c1694a7305f917a7fe3402f6b25c
bigrat95 <olivierbigrasmorin@hotmail.com>
2013-11-27 21:49:51 -0500

    change in ( age)
    changed the prompt function

--- CashRegister.js
+++ CashRegister.js
@@ -1,4 +1,34 @@
 function CashRegister(){
-var bigrat
+// JavaScript Document
+
+var i = 0;
+
+var cashRegister = { total:0, lastTransactionAmount: 0, add: function(itemCost){ this.total += 0; this.lastTransactionAmount = itemCost; }, scan: function(item,quantity){ switch (item){ case "cigarettes": this.add(10.55 * quantity); break; case "beer": this.add(2.50 * quantity); break; case "gum": this.add(1.99 * quantity); break; case "juice": this.add(2.45 * quantity); break; } return true; } }; //list of products in the store var inventory = ["cigarettes", "beer", "gum", "juice"]; for (){ if (); }else{ alert (Please scan again); }
+
+//Payment method
+
+var acceptedCards = ["visa", "mastercard", "amex", "discover"];
+
+var paymentMethod = ["debit", "acceptedCards", "cash"];
+
+//age restriction
+
+function Age(userAnswer)
+{ 
+	if (userAnswer === "yes") {
+	console.log("Allow purchase"); }
+	else{ 
+	console.log("Deny purchase"); 
+	}
+}
+
+var userAnswer = confirm("are you 18?")
+
+Age(userAnswer);
+
+
+//Points card
+
+var ptsCard = ["petro", "airmiles", "aeroplan", "essoextra"];
 
 }
